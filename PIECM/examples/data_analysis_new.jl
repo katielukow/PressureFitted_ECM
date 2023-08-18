342bb7c73233f85da8a4f01325f2f5ed79afe7db
Katie Lukow <caitlin.lukow@gmail.com>
2023-08-18 13:09:28 +0100

    plots

--- PIECM/examples/data_analysis_new.jl
+++ PIECM/examples/data_analysis_new.jl
@@ -6,28 +6,24 @@ pgfplotsx()
 Ϟ = distinguishable_colors(10)
 cell_dim = [0.0418, .1255]
 
-mbpf_0kpa = data_import_csv("data/HPPC/230606_MBPF_PCharact_Mel_SLPBA442124_0kpa_25C_Channel_3_Wb_1.csv", "new")
+P0kpa = data_import_csv("data/HPPC/230606_MBPF_PCharact_Mel_SLPBA442124_0kpa_25C_Channel_3_Wb_1.csv", "new")
 mbpf_50kpa = data_import_csv("data/HPPC/230606_MBPF_PCharact_Mel_SLPBA442124_50kpa_25C_Channel_4_Wb_1.csv", "new")
 mbpf_100kpa = data_import_csv("data/HPPC/230606_MBPF_PCharact_Mel_SLPBA442124_100kpa_25C_Channel_7_Wb_1.csv", "new")
 
+P0kpa.Date_Time .= replace.(P0kpa.Date_Time, "\t" => "")
 mbpf_50kpa.Date_Time .= replace.(mbpf_50kpa.Date_Time, "\t" => "")
 mbpf_100kpa.Date_Time .= replace.(mbpf_100kpa.Date_Time, "\t" => "")
 
-
+# ----------pressure data----------------
 
 A_cell = cell_dim[1] * cell_dim[2]
 Pres50 = pressure_dateformat_fix("data/PressureData/230606_MBPF_KL_50kpa.csv")
 P50kpa = pressurematch(mbpf_50kpa, Pres50, A_cell)
 
-
-
-
 Pres100 = pressure_dateformat_fix("data/PressureData/230606_MBPF_KL_100kpa.csv")
 P100kpa = pressurematch(mbpf_100kpa, Pres100, A_cell)
 
 p50_all = OrderedDict()
-
-
 for j in 1:20:101
     k = 1
     p50_plot = Array{Float64}(undef, 20, 2)
@@ -54,8 +50,6 @@ for j in 1:20:101
     p50_all[string(j)] = p50_plot
 end
 
-
-
 p100_all = OrderedDict()
 for j in 1:20:101
     k = 1
@@ -73,6 +67,92 @@ for j in 1:20:101
     
     p100_all[string(j)] = p100_plot
 end
+
+
+# -------------hppc data-----------------
+R0 = OrderedDict()
+for j in 1:20:101
+    if j == 1
+        R0[string(j)] = HPPC(P0kpa, 5, j, 19, 21, 24, 5, 15)
+    else
+        R0[string(j)] = HPPC(P0kpa,  5, j, 54, 56, 59, 50, 13)
+    end
+end
+
+R50 = OrderedDict()
+for j in 1:20:101
+    if j == 1
+        R50[string(j)] = HPPC(P50kpa, 5, j, 19, 21, 24, 5, 15)
+    else
+        R50[string(j)] = HPPC(P50kpa,  5, j, 54, 56, 59, 50, 13)
+    end
+end
+
+R100 = OrderedDict()
+for j in 1:20:101
+    if j == 1
+        R100[string(j)] = HPPC(P100kpa, 5, j, 19, 21, 24, 5, 15)
+    else
+        R100[string(j)] = HPPC(P100kpa,  5, j, 54, 56, 59, 50, 13)
+    end
+end
+
+
+pulse0_20SOC = hppc_fun(P0kpa, 10, 5, 1, 19, 21, 1)
+pulse50_20SOC = hppc_fun(P50kpa, 10, 5, 1, 19, 21, 1)
+pulse100_20SOC = hppc_fun(P100kpa, 10, 5, 1, 19, 21, 1)
+
+pulse0_90SOC = hppc_fun(P0kpa, 90, 5, 1, 19, 21, 1)
+pulse50_90SOC = hppc_fun(P50kpa, 90, 5, 1, 19, 21, 1)
+pulse100_90SOC = hppc_fun(P100kpa, 90, 5, 1, 19, 21, 1)
+
+# -------------capacity and energy data-----------------
+rptdis = 40
+rptchar = 44
+rpt0dis = 5
+rpt0char = 9
+rptdc = 46
+rpt0dc = 12
+dis_step = 34
+char_step = 36
+
+
+SOH_0 = Capacity_Fade(P0kpa, rpt0dis, rptdis)
+SOH_50 = Capacity_Fade(P50kpa, rpt0dis, rptdis)
+SOH_100 = Capacity_Fade(P100kpa, rpt0dis, rptdis)
+
+p50_discharge = sort!(filter(row -> row.Step_Index == dis_step_init, P50kpa), [:Date_Time])
+p50_charge = sort!(filter(row -> row.Step_Index == char_step_init, P50kpa), [:Date_Time])
+
+p0_discharge_all = OrderedDict()
+p0_dis_init = filter(row -> row.Step_Index == rpt0dis, P0kpa)
+pushfirst!(p0_dis_init, filter(row -> row.Step_Index == rpt0dis-1, P0kpa)[end,:])
+for i in 5:5:100
+    ptemp = filter(row -> row.Step_Index == dis_step && row.Cycle_Index == i, P0kpa)
+    pushfirst!(ptemp, filter(row -> row.Step_Index == dis_step-1 && row.Cycle_Index == i-1, P0kpa)[end,:])
+    p0_discharge_all[string(i)] = ptemp
+end
+
+p50_discharge_all = OrderedDict()
+p50_dis_init = sort!(filter(row -> row.Step_Index == rpt0dis, P50kpa), [:Date_Time])
+pushfirst!(p50_dis_init, filter(row -> row.Step_Index == rpt0dis-1, P50kpa)[end,:])
+for i in 5:5:100
+    ptemp = sort!(filter(row -> row.Step_Index == dis_step && row.Cycle_Index == i, P50kpa), [:Date_Time])
+    pushfirst!(ptemp, filter(row -> row.Step_Index == dis_step-1 && row.Cycle_Index == i-1, P50kpa)[end,:])
+    p50_discharge_all[string(i)] = ptemp
+end
+
+p100_discharge_all = OrderedDict()
+p100_dis_init = sort!(filter(row -> row.Step_Index == rpt0dis, P100kpa), [:Date_Time])
+pushfirst!(p100_dis_init, filter(row -> row.Step_Index == rpt0dis-1, P100kpa)[end,:])
+for i in 5:5:100
+    ptemp = sort!(filter(row -> row.Step_Index == dis_step && row.Cycle_Index == i, P100kpa), [:Date_Time])
+    pushfirst!(ptemp, filter(row -> row.Step_Index == dis_step-1 && row.Cycle_Index == i-1, P100kpa)[end,:])
+    p100_discharge_all[string(i)] = ptemp
+end
+
+
+# -------------plots-----------------
 
 P50_plot = @pgf Axis(
 
@@ -147,24 +227,271 @@ Pcomphppc_plot = @pgf Axis(
 
 )
 
+Rcomp_plot = @pgf Axis(
+
+    {
+        height = "7cm", width = "15cm",    
+        xlabel="State of Charge ["*L"\%"*"]",
+        ylabel="Resistance [mΩ]",
+        xmin = 0, 
+        xmax = 101,
+        # ymax = 15,
+        # ymin = 10,
+        xtick = 0:10:100,
+        legend_pos= "north east"
+    },
+
+    Plot({color = Ϟ[7], "thick", only_marks}, Table({x = "x", y = "y"}, x = round.(R0[1]["Discharge"][:,"SOC"],digits=0), y = R0[1]["Discharge"][:,"Resistance"].*1000)),
+    LegendEntry("0 kPa"),
+    Plot({color = Ϟ[5], "thick", only_marks}, Table({x = "x", y = "y"}, x = round.(R50[1]["Discharge"][:,"SOC"], digits=0), y = R50[1]["Discharge"][:,"Resistance"].*1000)),
+    LegendEntry("50 kPa"),
+    Plot({color = Ϟ[6], "thick", only_marks}, Table({x = "x", y = "y"}, x = round.(R100[1]["Discharge"][:,"SOC"], digits=0), y = R100[1]["Discharge"][:,"Resistance"].*1000)),
+    LegendEntry("100 kPa"),
+
+)
+
+R50_plot = @pgf Axis(
+
+    {
+        # height = "7cm", width = "15cm",    
+        # xlabel="State of Charge ["*L"\%"*"]",
+        # ylabel="Resistance [mΩ]",
+        # xmin = 0, 
+        # xmax = 101,
+        # ymax = 15,
+        # ymin = 10,
+        xtick = 0:10:100,
+        legend_pos= "north west"
+    },
+
+    Plot({color = Ϟ[4], "thick", only_marks}, Table({x = "x", y = "y"}, x = R50["1"]["Discharge"][:,:SOC], y = R50["1"]["Discharge"][:,:Resistance].*1000)),
+    LegendEntry("Cycle 1"),
+    Plot({color = Ϟ[5], "thick", only_marks}, Table({x = "x", y = "y"}, x = R50["21"]["Discharge"][:,:SOC], y = R50["21"]["Discharge"][:,:Resistance].*1000)),
+    LegendEntry("Cycle 21"),
+    Plot({color = Ϟ[6], "thick", only_marks}, Table({x = "x", y = "y"}, x = R50["41"]["Discharge"][:,:SOC], y = R50["41"]["Discharge"][:,:Resistance].*1000)),
+    LegendEntry("Cycle 41"),
+    Plot({color = Ϟ[7], "thick", only_marks}, Table({x = "x", y = "y"}, x = R50["61"]["Discharge"][:,:SOC], y = R50["61"]["Discharge"][:,:Resistance].*1000)),
+    LegendEntry("Cycle 61"),
+    Plot({color = Ϟ[8], "thick", only_marks}, Table({x = "x", y = "y"}, x = R50["81"]["Discharge"][:,:SOC], y = R50["81"]["Discharge"][:,:Resistance].*1000)),
+    LegendEntry("Cycle 81"),
+    Plot({color = Ϟ[10], "thick", only_marks}, Table({x = "x", y = "y"}, x = R50["101"]["Discharge"][:,:SOC], y = R50["101"]["Discharge"][:,:Resistance].*1000)),
+    LegendEntry("Cycle 101"),
+
+)
+
+
+Pulse15_plot = @pgf GroupPlot(
+
+
+    {
+        group_style =
+        {
+            group_size="2 by 1",
+            xticklabels_at="edge bottom",
+            # yticklabels_at="edge left",
+            # legend_pos= "north west"
+        },
+        height = "8cm", width = "10cm",    
+
+    },
+    {
+        xlabel="Time [s]",
+        ylabel="Voltage [V]",
+        xmin = -0.1, 
+        xmax = 101,
+        ymin = 3.35,
+        ymax = 3.8,
+        xtick = 0:20:100,
+        # legend_pos= "south east"
+    },
+
+    Plot({color = Ϟ[7], "thick"}, Table({x = "x", y = "y"}, x = pulse0_20SOC[:,"Test_Time(s)"], y = pulse0_20SOC[:,"Voltage(V)"])),
+    # LegendEntry("0 kPa"),
+    Plot({color = Ϟ[5], "thick"}, Table({x = "x", y = "y"}, x = pulse50_20SOC[:,"Test_Time(s)"], y = pulse50_20SOC[:,"Voltage(V)"])),
+    # LegendEntry("50 kPa"),
+    Plot({color = Ϟ[6], "thick"}, Table({x = "x", y = "y"}, x = pulse100_20SOC[:,"Test_Time(s)"], y = pulse100_20SOC[:,"Voltage(V)"])),
+    # LegendEntry("100 kPa"),
+    {
+        xlabel="Time [s]",
+        # ylabel="Voltage [V]",
+        xmin = -0.1, 
+        xmax = 101,
+        ymin = 3.75,
+        ymax = 4.2,
+        xtick = 0:20:100,
+        legend_pos= "south east"
+    },
+
+    Plot({color = Ϟ[7], "thick"}, Table({x = "x", y = "y"}, x = pulse0_90SOC[:,"Test_Time(s)"], y = pulse0_90SOC[:,"Voltage(V)"])),
+    LegendEntry("0 kPa"),
+    Plot({color = Ϟ[5], "thick"}, Table({x = "x", y = "y"}, x = pulse50_90SOC[:,"Test_Time(s)"], y = pulse50_90SOC[:,"Voltage(V)"])),
+    LegendEntry("50 kPa"),
+    Plot({color = Ϟ[6], "thick"}, Table({x = "x", y = "y"}, x = pulse100_90SOC[:,"Test_Time(s)"], y = pulse100_90SOC[:,"Voltage(V)"])),
+    LegendEntry("100 kPa"),
+
+)
+
+Pulsezoom_plot = @pgf GroupPlot(
+
+
+    {
+        group_style =
+        {
+            group_size="2 by 1",
+            xticklabels_at="edge bottom",
+            # yticklabels_at="edge left",
+            # legend_pos= "north west"
+        },
+        height = "8cm", width = "10cm",    
+
+    },
+    {
+        xlabel="Time [s]",
+        ylabel="Voltage [V]",
+        xmin = -0.1, 
+        xmax = 14,
+        ymin = 3.35,
+        ymax = 3.8,
+        xtick = 0:2:100,
+        # legend_pos= "south east"
+    },
+
+    Plot({color = Ϟ[7], "thick"}, Table({x = "x", y = "y"}, x = pulse0_20SOC[:,"Test_Time(s)"], y = pulse0_20SOC[:,"Voltage(V)"])),
+    # LegendEntry("0 kPa"),
+    Plot({color = Ϟ[5], "thick"}, Table({x = "x", y = "y"}, x = pulse50_20SOC[:,"Test_Time(s)"], y = pulse50_20SOC[:,"Voltage(V)"])),
+    # LegendEntry("50 kPa"),
+    Plot({color = Ϟ[6], "thick"}, Table({x = "x", y = "y"}, x = pulse100_20SOC[:,"Test_Time(s)"], y = pulse100_20SOC[:,"Voltage(V)"])),
+    # LegendEntry("100 kPa"),
+    {
+        xlabel="Time [s]",
+        # ylabel="Voltage [V]",
+        xmin = -0.1, 
+        xmax = 14,
+        ymin = 3.75,
+        ymax = 4.2,
+        xtick = 0:2:100,
+        legend_pos= "south east"
+    },
+
+    Plot({color = Ϟ[7], "thick"}, Table({x = "x", y = "y"}, x = pulse0_90SOC[:,"Test_Time(s)"], y = pulse0_90SOC[:,"Voltage(V)"])),
+    LegendEntry("0 kPa"),
+    Plot({color = Ϟ[5], "thick"}, Table({x = "x", y = "y"}, x = pulse50_90SOC[:,"Test_Time(s)"], y = pulse50_90SOC[:,"Voltage(V)"])),
+    LegendEntry("50 kPa"),
+    Plot({color = Ϟ[6], "thick"}, Table({x = "x", y = "y"}, x = pulse100_90SOC[:,"Test_Time(s)"], y = pulse100_90SOC[:,"Voltage(V)"])),
+    LegendEntry("100 kPa"),
+
+)
+
+SOH_plot = @pgf Axis(
+
+
+    # {
+    #     group_style =
+    #     {
+    #         group_size="2 by 1",
+    #         xticklabels_at="edge bottom",
+    #         # yticklabels_at="edge left",
+    #         # legend_pos= "north west"
+    #     },
+    #     height = "8cm", width = "10cm",    
+
+    # },
+    {
+        height = "8cm", width = "15cm",
+        xlabel="Cycle",
+        ylabel="State-of-Health ["*L"\%"*"]",
+        xmin = -0.1, 
+        xmax = 101,
+        # ymin = 0.975,
+        # ymax = 1.005,
+        # ytick = 0.98:0.01:1.00,
+        xtick = 0:10:100,
+        legend_pos= "north east"
+    },
+
+    Plot({color = Ϟ[7], "thick"}, Table({x = "x", y = "y"}, x = SOH_0[:,1], y = SOH_0[:,2])),
+    LegendEntry("0 kPa"),
+    Plot({color = Ϟ[5], "thick"}, Table({x = "x", y = "y"}, x = SOH_50[:,1], y = SOH_50[:,2])),
+    LegendEntry("50 kPa"),
+    Plot({color = Ϟ[6], "thick"}, Table({x = "x", y = "y"}, x = SOH_100[:,1], y = SOH_100[:,2])),
+    LegendEntry("100 kPa"),
+
+
+)
+
+dis_plot = @pgf GroupPlot(
+
+
+    {
+        group_style =
+        {
+            group_size="1 by 2",
+            xticklabels_at="edge bottom",
+            yticklabels_at="edge left",
+            # legend_pos= "north west"
+        },
+        height = "8cm", width = "20cm",    
+
+    },
+    {
+        # height = "8cm", width = "15cm",
+        # xlabel="Cycle",
+        ylabel="Voltage [V]",
+        xmin = -0.1, 
+        xmax = 6,
+        # ymin = 0.975,
+        # ymax = 1.005,
+        # ytick = 0.98:0.01:1.00,
+        # xtick = 0:0.5:6,
+        legend_pos= "north east"
+    },
+
+    Plot({color = Ϟ[7], "thick"}, Table({x = "x", y = "y"}, x = p0_dis_init[:,"Discharge_Capacity(Ah)"], y = p0_dis_init[:,"Voltage(V)"])),
+    LegendEntry("0 kPa"),
+    Plot({color = Ϟ[5], "thick"}, Table({x = "x", y = "y"}, x = p50_dis_init[:,"Discharge_Capacity(Ah)"], y = p50_dis_init[:,"Voltage(V)"])),
+    LegendEntry("50 kPa"),
+    Plot({color = Ϟ[6], "thick"}, Table({x = "x", y = "y"}, x = p100_dis_init[:,"Discharge_Capacity(Ah)"], y = p100_dis_init[:,"Voltage(V)"])),
+    LegendEntry("100 kPa"),
+    
+    {
+        # height = "8cm", width = "15cm",
+        xlabel="Discharge Capacity [Ah]",
+        ylabel="Voltage [V]",
+        xmin = -0.1, 
+        xmax = 6,
+        # ymin = 0.975,
+        # ymax = 1.005,
+        # ytick = 0.98:0.01:1.00,
+        xtick = 0:0.5:6,
+        legend_pos= "north east"
+    },
+
+    Plot({color = Ϟ[7], "thick"}, Table({x = "x", y = "y"}, x = p0_discharge_all["5"][:,"Discharge_Capacity(Ah)"], y = p0_discharge_all["5"][:,"Voltage(V)"])),
+    LegendEntry("0 kPa"),
+    Plot({color = Ϟ[5], "thick"}, Table({x = "x", y = "y"}, x = p50_discharge_all["5"][:,"Discharge_Capacity(Ah)"], y = p50_discharge_all["5"][:,"Voltage(V)"])),
+    LegendEntry("50 kPa"),
+    Plot({color = Ϟ[6], "thick"}, Table({x = "x", y = "y"}, x = p100_discharge_all["5"][:,"Discharge_Capacity(Ah)"], y = p100_discharge_all["5"][:,"Voltage(V)"])),
+    LegendEntry("100 kPa"),
+
+
+)
+
 pgfsave("figures/P50_plot.pdf",
         P50_plot)
 
 pgfsave("figures/Pcomp_plot.pdf",
 Pcomp_plot)
 
-dis_step_init = 5
-char_step_init = 9
-dis_step = 34
-char_step = 36
+pgfsave("figures/Rcomp_plot.pdf",
+Rcomp_plot)
+pgfsave("figures/Pulse15_plot.pdf",
+Pulse15_plot)
+pgfsave("figures/SOH_plot.pdf",
+SOH_plot)
+pgfsave("figures/dis_plot.pdf",
+dis_plot)
 
-p50_discharge = sort!(filter(row -> row.Step_Index == dis_step_init, P50kpa), [:Date_Time])
-p50_charge = sort!(filter(row -> row.Step_Index == char_step_init, P50kpa), [:Date_Time])
 
-p50_discharge_all = OrderedDict()
-for i in 1:101
-    p50_discharge_all[string(i)] = filter(row -> row.Step_Index == dis_step && row.Cycle_Index == i, P50kpa)
-end
 
 plot(p50_discharge[:,"Discharge_Capacity(Ah)"], p50_discharge[:,"Pressure"], label = "C/2", xlabel = "Time [s]", ylabel = "Voltage [V]")
 plot!(p50_discharge_all["1"][:,"Discharge_Capacity(Ah)"], p50_discharge_all["1"][:,"Pressure"], label = "1", xlabel = "Time [s]", ylabel = "Voltage [V]")
--- PIECM/src/PIECM.jl
+++ PIECM/src/PIECM.jl
@@ -1,10 +1,10 @@
 module PIECM
 
-using CSV, DataFrames, Dates, Infiltrator, JLD2, Interpolations, XLSX, Statistics 
+using CSV, DataFrames, Dates, Infiltrator, JLD2, Interpolations, XLSX, Statistics, DataStructures
 using StatsBase: L2dist
  
 export data_import_csv, data_import_excel, pressure_dateformat_fix, pressurematch, hppc_pulse, pocv, sqrzeros, HPPC, hppc_fun
-export ecm_discrete, costfunction, HPPC_n, data_imp, pres_avg
+export ecm_discrete, costfunction, HPPC_n, data_imp, pres_avg, Capacity_Fade
 
 # --------------- Fitting data import and filtering -----------------------------
 
@@ -20,6 +20,7 @@ function data_import_csv(file_name, format)
 		rename!(df,"Current (A)" => "Current(A)")
 		rename!(df,"Internal Resistance (Ohm)" => "Internal_Resistance(Ohm)")
 		rename!(df,"Discharge Capacity (Ah)" => "Discharge_Capacity(Ah)")
+		rename!(df,"Discharge Energy (Wh)" => "Discharge_Energy(Wh)")
 		rename!(df,"Charge Capacity (Ah)" => "Charge_Capacity(Ah)")
 	end
 
@@ -176,62 +177,6 @@ function HPPC(data, soc_increment, cycle, dis_pulse_step, char_pulse_step, dis_s
 	return γ
 end
 
-function HPPC(data, soc_increment, cycle, dis_pulse_step, char_pulse_step, dis_step, initial_cap_step, DCIR_step, new)
-
-    γ = Dict()
-
-	initial_capacity = filter(row -> row."Step Index" == initial_cap_step, data)[end,"Discharge Capacity (Ah)"]
-
-	DCIR = unique(select(filter(row -> row."Step Index" == DCIR_step, data), "Internal Resistance (Ohm)"))
-
-	soc_steps = 100 ÷ soc_increment #length(collect(0:soc_increment:100))-1
-	SOC = Array{Float64}(undef, soc_steps, 1)
-	∇_discharge = DataFrame([[], [], [], [], [], [], [], [], []], ["SOC","Start Voltage(V)","End Voltage(V)", "Resistance", "Average Power (W)", "Max Power (W)", "Min Power (W)", "Max Current(A)", "Min Current(A)"])	
-	∇_charge = DataFrame([[], [], [], [], [], [], [], [], []], ["SOC","Start Voltage(V)","End Voltage(V)", "Resistance", "Average Power (W)", "Max Power (W)", "Min Power (W)", "Max Current(A)", "Min Current(A)"])
-
-	dpulse_v = filter(row -> row."Step Index" == (dis_pulse_step-1) && row."Cycle Index" == cycle, data)
-	cpulse_v = filter(row -> row."Step Index" == (char_pulse_step-1) && row."Cycle Index" == cycle, data)
-	df_dpulse = filter(row -> row."Step Index" == dis_pulse_step && row."Cycle Index" == cycle, data) 
-	df_cpulse = filter(row -> row."Step Index" == char_pulse_step && row."Cycle Index" == cycle, data)
-	df_discharge = filter(row -> row."Step Index" == dis_step && row."Cycle Index" == cycle, data)
-
-	# Calculate State of Charge based on capacities - Discharge to next step + discharge pulse - charge pulse
-	# for g in 0:(soc_steps-1) 0:findmax(df_discharge[:,"TC_Counter1"])
-
-
-	for g in 1:(soc_steps-1)
-		o = filter(row -> row."TC_Counter1" == g, df_discharge)
-		dis_step_cap = o[end, "Discharge Capacity (Ah)"] - o[1, "Discharge Capacity (Ah)"]
-		k = filter(row -> row."TC_Counter1" == g, df_dpulse)
-		pulse_cap_d = k[end, "Discharge Capacity (Ah)"] - k[1, "Discharge Capacity (Ah)"]
-		p = filter(row -> row."TC_Counter1" == g, df_cpulse)
-		pulse_cap_c = p[end, "Charge Capacity (Ah)"] - p[1, "Charge Capacity (Ah)"]
-
-		SOC[g+1] = dis_step_cap + pulse_cap_d - pulse_cap_c
-	end
-
-	# print(SOC)
-	SOC = cumsum(SOC[:,1])
-	SOC = 100 .- SOC ./ initial_capacity .* 100
-
-	# Calculate relevant data for each SOC point
-	for j in 1:(soc_steps)
-		dpulse_vi = filter(row -> row."TC_Counter1" == j-1, dpulse_v)[end,"Voltage (V)"]
-		discharge = hppc_calc_n(df_dpulse, j, dpulse_vi)
-		push!(∇_discharge, [SOC[j], dpulse_vi, filter(row -> row."TC_Counter1" == j-1, df_dpulse)[end,"Voltage (V)"],  discharge[1], discharge[2], discharge[3], discharge[4], discharge[5], discharge[6]])
-	end
-
-	for q in 1:(soc_steps)
-		cpulse_vi = filter(row -> row."TC_Counter1" == q-1, cpulse_v)[end,"Voltage (V)"]
-		charge = hppc_calc_n(df_cpulse, q, cpulse_vi)
-		push!(∇_charge, [SOC[q], cpulse_vi, filter(row -> row."TC_Counter1" == q-1, df_cpulse)[end,"Voltage (V)"], charge[1], charge[2], charge[3], charge[4], charge[5], charge[6]])
-	end
-
-	γ = Dict("Charge" => ∇_charge, "Discharge" => ∇_discharge)
-
-	return γ
-end
-
 function hppc_calc(dataframe, i, init_V)
 	df = filter(row -> row."TC_Counter1" == (i-1), dataframe)
 
@@ -252,24 +197,25 @@ function hppc_calc(dataframe, i, init_V)
 	return [r, P_avg, t[1], t[2], t[3], t[4]]
 end
 
-function hppc_calc_n(dataframe, i, init_V)
-	df = filter(row -> row."TC_Counter1" == (i-1), dataframe)
 
-	r = abs((init_V - df[end,"Voltage (V)"]) / abs(mean(df[:,"Current (A)"])))
-	P = df[:,"Voltage (V)"] .* df[:,"Current (A)"]
-	P_min = findmin(abs.(P))[1]
-	P_max = findmax(abs.(P))[1]
-	P_avg = mean(P)
-	I_min = findmin(abs.(df[:,"Current (A)"]))[1]
-	I_max = findmax(abs.(df[:,"Current (A)"]))[1]
 
-	t = [P_max P_min I_max I_min]
+function Capacity_Fade(df, d_stepinit, d_step)
+	SOH = Array{Float64}(undef, 6, 3)
+	
+	discharge = filter(row -> row.Step_Index == d_step, df)
+	capinit = filter(row -> row.Step_Index == d_stepinit, df)[end, "Discharge_Capacity(Ah)"] - filter(row -> row.Step_Index == d_stepinit, df)[1, "Discharge_Capacity(Ah)"]
+	energyinit = filter(row -> row.Step_Index == d_stepinit, df)[end, "Discharge_Energy(Wh)"] - filter(row -> row.Step_Index == d_stepinit, df)[1, "Discharge_Energy(Wh)"]
+	SOH[1,:] = [0.0,100.0, energyinit]
+	j = 1
 
-	if df[1, "Current (A)"] < 0
-		t .= -t
+	for i in 21:20:101
+		Qtemp = filter(row -> row.Cycle_Index == i, discharge)[end, "Discharge_Capacity(Ah)"] - filter(row -> row.Cycle_Index == i, discharge)[1, "Discharge_Capacity(Ah)"]
+		Etemp = filter(row -> row.Cycle_Index == i, discharge)[end, "Discharge_Energy(Wh)"] - filter(row -> row.Cycle_Index == i, discharge)[1, "Discharge_Energy(Wh)"]
+		SOH[j+1,:] = [i, Qtemp / capinit * 100, Etemp]
+		j += 1
 	end
 
-	return [r, P_avg, t[1], t[2], t[3], t[4]]
+	return SOH
 end
 
 function hppc_fun(pd, soc, soc_step, pulse_rate, dis_step, char_step, cycle_index)
@@ -278,7 +224,7 @@ function hppc_fun(pd, soc, soc_step, pulse_rate, dis_step, char_step, cycle_inde
     hppc = filter(row -> row."Cycle_Index" == cycle_index, hppc)
     hppc."Test_Time(s)" .-= hppc."Test_Time(s)"[1]
 
-    return hppc
+    return sort!(hppc,"Date_Time")
 end
 # ----------------------------------
 # x = [Rᵢ, Cᵢ, R₀]
