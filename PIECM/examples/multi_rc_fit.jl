ea4d7a2277c97f6cfb719aca0a72a2f127bd93dc
Brady Planden <brady.planden@gmail.com>
2023-11-28 22:18:42 +0000

    100% 2 RC Run

--- PIECM/examples/multi_rc_fit.jl
+++ PIECM/examples/multi_rc_fit.jl
@@ -28,19 +28,19 @@ function ecm_err_range_2RC(data, Q, ocv, soc, soc_increment, dstep, C1_range, R1
     for (k,r0) in enumerate(zrng)
         print(k, "\n")
         df = DataFrame([[],[],[],[],[]], [:R1, :R2, :C1, :C2, :Err])
-        for (i,r1) in enumerate(xrng)
-            for (j,c1) in enumerate(yrng)
-                for (m,r2) in enumerate(x2rng)
-                    for (n,c2) in enumerate(y2rng)
+        for (r1) in R1_range
+            for (c1) in C1_range
+                for (r2) in R1_range
+                    for (c2) in C1_range
                         v_model = ecm_discrete([r1,r2, c1,c2, r0], 2, current, test_time, 0.999, Q, ocv, soc)
                         err = sqL2dist(v_model, voltage_end)
-                        err_matrix[i+1, j+1] = err
+                        # err_matrix[i+1, j+1] = err
                         push!(df, [r1, r2, c1, c2, err])
                     end
                 end
             end
         end
-        
+
         errors[round(r0, digits = 6)] = df
         # errors[round(r0, digits = 6)] = copy(err_matrix)
     end
@@ -48,16 +48,17 @@ function ecm_err_range_2RC(data, Q, ocv, soc, soc_increment, dstep, C1_range, R1
     return errors, df
 end
 
-mbpf25kpa = data_import_csv("data/HPPC/230320_MBPF_Investigation_25kpa_11_0043_Channel_6_Wb_1.csv", "old")
-ocv1 = pocv("data/OCV/220310_BTC_POCV_GITT_Mel_SLPB7336128HV_1_25C_Channel_5_Wb_1.csv", 5, 8, 1000)
+mbpf25kpa = data_import_csv("data/HPPC/230320_MBPF_Investigation_25kpa_11_0043_Channel_6_Wb_1.CSV", "old")
+mbpf40kpa = data_import_csv("data/HPPC/230320_MBPF_Investigation_40kpa_11_0048_Channel_6_Wb_1.CSV", "old")
+ocv1 = pocv("data/OCV/220310_BTC_POCV_GITT_Mel_SLPB7336128HV_1_25C_Channel_5_Wb_1.CSV", 5, 8, 1000)
 
-err25, df25 = ecm_err_range_2RC(mbpf25kpa, 3.7, ocv1, 1, 10, 17, [15000, 100, 25000], [0.00001, .0005, 0.005])
+err40, df40 = ecm_err_range_2RC(mbpf40kpa, 3.7, ocv1, 1, 10, 20, LinRange(10000,30000,101), LinRange(0.00001, .010001, 11))
 
-mins25 = DataFrame([[],[],[],[],[],[]], [:Err, :R0, :R1, :R2, :C1, :C2])
+mins40 = DataFrame([[],[],[],[],[],[]], [:Err, :R0, :R1, :R2, :C1, :C2])
 
-for i in eachindex(err25)
-    mini = argmin(err25[i][:, :Err])
-    temp = err25[i][mini, :]
+for i in eachindex(err40)
+    mini = argmin(err40[i][:, :Err])
+    temp = err40[i][mini, :]
     # print(temp[:R1], "\n")
-    push!(mins25, [temp[:Err], i, temp[:R1], temp[:R2], temp[:C1], temp[:C2]])
+    push!(mins40, [temp[:Err], i, temp[:R1], temp[:R2], temp[:C1], temp[:C2]])
 end
